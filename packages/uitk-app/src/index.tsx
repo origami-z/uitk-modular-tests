3eb77e19d75b31d6335e5e4c76d77bb9862016e4
origami-z <davidcui2@me.com>
2022-05-25 11:11:51 +0100

    Fix react 18 errror

--- packages/uitk-app/src/index.tsx
+++ packages/uitk-app/src/index.tsx
@@ -1,18 +1,17 @@
-import { ToolkitProvider } from "@jpmorganchase/uitk-core";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
+import { ToolkitProvider } from '@jpmorganchase/uitk-core';
 // Import UITK CSS before any component is loaded to get correct CSS order
-import "@jpmorganchase/uitk-theme/index.css";
+import '@jpmorganchase/uitk-theme/index.css';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <ToolkitProvider>
       <App />
     </ToolkitProvider>
   </React.StrictMode>,
-  document.getElementById("root")
 );
